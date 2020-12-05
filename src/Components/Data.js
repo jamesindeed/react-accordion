2b170fb222f0d0191f471a29d0bed9a09fe932e0
James Troughton <jamesstroughton@gmail.com>
2020-12-05 14:28:41 +1300

    First commit

---
+++ src/Components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+    {
+        question: 'What is my first name?',
+        answer: 'James'
+    },
+    {
+        question: 'What is my middle name?',
+        answer: 'Samuel'
+    },
+    {
+        question: 'What is my last name?',
+        answer: 'Troughton'
+    }
+];
